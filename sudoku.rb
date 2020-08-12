c74e9c94830acf0cbbccede3f16e2a4f1151f835
Linda Song <linda.songyaoling@gmail.com>
2020-08-11 21:06:41 -0400

    fixed the error

--- sudoku.rb
+++ sudoku.rb
@@ -1,3 +1,5 @@
+require "colorize"
+
 class Board
   def self.empty_grid
     Array.new(9) do
@@ -109,6 +111,7 @@ class Tile
       @value = new_value
     end
   end
+end
 
 class SudokuGame
   def self.from_file(filename)
